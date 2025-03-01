da732495d65de4898cc4b3919481920d3113f9b9
Satyam Gupta <satyamgupta3803@gmail.com>
2025-03-01 19:05:47 +0530

    Initialised react project, understood react folder structure

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
+import AppNew from './AppNew';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppNew);
